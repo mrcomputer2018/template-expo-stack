47a48eaef1f99bf45c8a350b9e77b0fe33c5366f
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-09-23 20:59:44 -0300

    Update dependencies for @react-navigation/native-stack

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './screens/Home';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
-        <NavigationContainer children={undefined}>
-            {/* Rest of your app code */}
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={Home} />
+            </Stack.Navigator>
         </NavigationContainer>
     );
 }
+
