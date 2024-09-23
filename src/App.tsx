51398c6acbeee8e2e51bace22bc604af0fa0b873
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-09-23 20:54:07 -0300

    Refactor editorconfig and update dependencies

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+export default function App() {
+    return (
+        <NavigationContainer children={undefined}>
+            {/* Rest of your app code */}
+        </NavigationContainer>
+    );
+}
