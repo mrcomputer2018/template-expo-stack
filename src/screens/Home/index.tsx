d1400c014e6384ba6830c9681373697f8664c2c2
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-09-23 21:02:06 -0300

    Add StatusBar component to Home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
 import { View, Text } from 'react-native';
 
 export default function Home() {
     return (
         <View>
+            <StatusBar style="auto" />
+            
             <Text>Welcome to the Home screen!</Text>
         </View>
     );
